aeeacaac2bdded93685f323970d93e007a47889d
peekinternational <peekinternational@gmail.com>
2022-03-02 11:52:59 +0500

    change logo link

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -28,7 +28,7 @@
                         <div class="brand-logo pb-4 text-center">
                             <a href="{{url('customer/login')}}" class="logo-link">
                                 <img class="logo-light logo-img logo-img-lg" src="{{asset('/images/logo.png') }}" srcset="{{asset('/images/logo2x.png 2x') }}" alt="logo">
-                                <img class="logo-dark logo-img logo-img-lg" src="./images/logo-dark.png" srcset="{{asset('/images/logo-dark2x.png 2x') }}" alt="logo-dark">
+                                <img class="logo-dark logo-img logo-img-lg" src="{{asset('images/logo-dark.png') }}" srcset="{{asset('/images/logo-dark2x.png 2x') }}" alt="logo-dark">
                             </a>
                         </div>
                         <div class="card">
